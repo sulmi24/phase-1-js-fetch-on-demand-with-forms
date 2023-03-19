c793bbb6beb6802c2195ace1b0beb4c591e28ac0
sulmi24 <a.sulmi24@gmail.com>
2023-03-19 17:16:58 +0200

    Solved : Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let input = document.querySelector("input#searchByID");
+
+    console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        title.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
